b1e536b761a433aba66fc570cb02605bf29588ed
fyxld <113503407+fyxld@users.noreply.github.com>
2022-09-14 16:16:46 +0800

    更新 pxt.json, enums.d.ts, _locales/zh/PCF8563-strings.json

---
+++ enums.d.ts
@@ -0,0 +1,16 @@
+declare const enum pcf8563_type_e {
+    //% block="year"
+    kTypeYear = 1,
+    //% block="month"
+    kTypeMonth = 2,
+    //% block="day"
+    kTypeDay = 3,
+    //% block="weekday"
+    kTypeWeekday = 4,
+    //% block="hour"
+    kTypeHour = 5,
+    //% block="minute"
+    kTypeMinute = 6,
+    //% block="sec"
+    kTypeSec = 7
+}
